9bbfe0129dc00bcb5ca6688edaaf4901684f9d55
Seerasak Tajai <64143109@g.cmru.ac.th>
2025-01-12 16:42:39 +0700

    awda

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+require('dotenv').config(); 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: process.env.API_BASE_URL || 'http://localhost:4000/api',
   headers: {
     'Content-Type': 'application/json',
   }
