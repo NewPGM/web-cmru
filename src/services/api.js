ba940da4b06676ac598e199f8abe8bc66f22cd33
Seerasak Tajai <64143109@g.cmru.ac.th>
2025-01-13 09:11:17 +0700

    ww

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:4000/api',
+  baseURL: process.env.VUE_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
