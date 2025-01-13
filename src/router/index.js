cae5ec510bf871898b63c48888e3b85c57eeed1c
Seerasak Tajai <64143109@g.cmru.ac.th>
2025-01-13 08:58:46 +0700

    new

--- src/router/index.js
+++ src/router/index.js
@@ -12,102 +12,122 @@ import AdminLogin from '@/views/Admin.vue'
 // import Admin_main from '@/views/app.vue'
 import adminManagement from '@/views/AdminManagement.vue'
 
-const routes = [
-  {
-    path: '/',
-    redirect: '/login',
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: AdminLogin,
-  },
-  {
-    path: '/app',
-    name: 'App_main',
-    // component: Admin_main,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/instructors',
-    name: 'instructors',
-    component: InstructorManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/rooms',
-    name: 'rooms',
-    component: RoomManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/locations',
-    name: 'locations',
-    component: LocationManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/philosophy',
-    name: 'philosophy',
-    component: PhilosophynManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/courses',
-    name: 'courses',
-    component: coursesManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/degree_programs',
-    name: 'degree_programs',
-    component: DegreeprogramsManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/instructor_sj',
-    name: 'instructor_sj',
-    component: instructor_sjManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/program_subjects',
-    name: 'program_subjects',
-    component: program_subjectsManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/training_phrases',
-    name: 'training_phrases',
-    component: training_phrasesManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    component: adminManagement,
-    meta: { requiresAuth: true }, // ต้องล็อกอิน
-  }
-]
+
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes : [
+    {
+      path: '/',
+      name: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: AdminLogin,
+    },
+    {
+      path: '/app',
+      name: 'App_main',
+      // component: Admin_main,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/instructors',
+      name: 'instructors',
+      component: InstructorManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/rooms',
+      name: 'rooms',
+      component: RoomManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/locations',
+      name: 'locations',
+      component: LocationManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/philosophy',
+      name: 'philosophy',
+      component: PhilosophynManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/courses',
+      name: 'courses',
+      component: coursesManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/degree_programs',
+      name: 'degree_programs',
+      component: DegreeprogramsManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/instructor_sj',
+      name: 'instructor_sj',
+      component: instructor_sjManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/program_subjects',
+      name: 'program_subjects',
+      component: program_subjectsManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/training_phrases',
+      name: 'training_phrases',
+      component: training_phrasesManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: adminManagement,
+      meta: { requiresAuth: true }, // ต้องล็อกอิน
+    }
+  ]
 })
+
+// router.beforeEach((to, from, next) => {
+//   const isAuthenticated = localStorage.getItem('admin-token') !== null
+
+//   if (to.meta.requiresAuth && !isAuthenticated) {
+//     // เปลี่ยนไปหน้า Login ถ้าไม่ได้ล็อกอิน
+//     next('/login')
+//   } else if (to.path === '/login' && isAuthenticated) {
+//     // ถ้าล็อกอินแล้ว ให้เปลี่ยนไปหน้าแรก (เช่น /app)
+//     next('/app')
+//   } else {
+//     // อนุญาตให้เข้าถึงเส้นทางอื่น ๆ
+//     next()
+//   }
+// })
 
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = localStorage.getItem('admin-token') !== null
+  const isAuthenticated = localStorage.getItem('admin-token') !== null;
 
-  if (to.meta.requiresAuth && !isAuthenticated) {
+  // ถ้าผู้ใช้รีหน้าและไม่มีเส้นทางที่กำหนด ให้ส่งกลับไปที่ '/'
+  if (!to.matched.length) {
+    next('/login');
+  } else if (to.meta.requiresAuth && !isAuthenticated) {
     // เปลี่ยนไปหน้า Login ถ้าไม่ได้ล็อกอิน
-    next('/login')
+    next('/login');
   } else if (to.path === '/login' && isAuthenticated) {
     // ถ้าล็อกอินแล้ว ให้เปลี่ยนไปหน้าแรก (เช่น /app)
-    next('/app')
+    next('/app');
   } else {
     // อนุญาตให้เข้าถึงเส้นทางอื่น ๆ
-    next()
+    next();
   }
-})
+});
+
 
 export default router;
